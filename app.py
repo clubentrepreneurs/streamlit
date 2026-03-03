e8001d8a27b91bb2feea2080840228c8f97c7267
clubentrepreneurs <clubucaentrepreneurs@gmail.com>
2026-03-03 14:15:39 +0100

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Qdrant
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
 import qdrant_client
